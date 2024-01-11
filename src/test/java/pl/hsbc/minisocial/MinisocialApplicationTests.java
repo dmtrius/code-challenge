3677cad1b360323b9a3d0dadbe43f9a47d97b17f
Dmytro Gordiienko <dmitry.gordienko@gmail.com>
2024-01-11 11:40:42 +0100

    initial commit

---
+++ src/test/java/pl/hsbc/minisocial/MinisocialApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.hsbc.minisocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinisocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
